0b9d8acf39e62031fd3567b6ef16924f3a3de7c3
Teodor Danielsen <teodor.danielsen@gmail.com>
2019-12-01 23:01:22 +0100

    design changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import {Title} from '@angular/platform-browser';
+import {Location} from '@angular/common';
+import {environment} from '../environments/environment';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   
+  title = "PureOKRs";
+
+  location: Location;
+
   constructor(private titleService: Title){
     
   }
-  title = "PureOKRs";
+
+  ngOnInit(){
+    if (environment.production) {
+      if (location.protocol === 'http:') {
+       window.location.href = location.href.replace('http', 'https');
+      }
+     }
+  }
+  
 }
