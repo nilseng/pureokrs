0d3892b5fab1d22dbfff89f9620b36bd35cad3da
Teodor Danielsen <teodor.danielsen@gmail.com>
2019-12-06 22:59:43 +0100

    added edit OKR modal

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,7 +6,6 @@ import { AuthGuardService } from './auth-guard.service';
 import {LoginComponent} from './login/login.component';
 import {RegisterComponent} from './register/register.component';
 import {CompanyComponent} from './company/company.component';
-import {EditOkrComponent} from './okr/edit-okr/edit-okr.component';
 import { UsersComponent } from './users/users.component';
 import {ResetpasswordComponent} from './login/resetpassword/resetpassword.component';
 import { NewPasswordComponent } from './login/new-password/new-password.component';
@@ -21,7 +20,6 @@ const routes: Routes = [
   {path: 'register', component: RegisterComponent},
   {path: 'company/okrs', component: CompanyComponent, canActivate: [AuthGuardService], resolve: {okrs: OkrResolver}},
   {path: 'okr-tree', component: OkrTreeComponent, canActivate: [AuthGuardService]},
-  {path: 'edit-okr/:id', component: EditOkrComponent, canActivate: [AuthGuardService]},
   {path: 'company/users', component: UsersComponent, canActivate: [AuthGuardService], resolve: {users: UsersResolver}},
   {path: 'resetpassword', component: ResetpasswordComponent},
   {path: 'resetpassword/:email/:token', component: NewPasswordComponent},
