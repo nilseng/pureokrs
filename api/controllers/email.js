591f5845d200f71b0b66eb4ef9dc285cf5fb5b8c
Teodor Danielsen <teodor.danielsen@gmail.com>
2020-01-10 19:20:07 +0100

    added styling to Add User Email

--- api/controllers/email.js
+++ api/controllers/email.js
@@ -11,32 +11,71 @@ module.exports.sendEmail = (email, companyName) => {
   msg.subject = companyName + ' is registered. Welcome!';
   msg.to = email;
   sgMail.setApiKey(process.env.SENDGRID_API_KEY);
-  sgMail.send(msg, (err, doc) =>{});
+  sgMail.send(msg, (err, doc) => { });
   return;
 }
 
 module.exports.sendNewUserEmail = (email, companyName, token) => {
   let msg = {
     from: 'teodor.nilseng@gmail.com',
-    text: 'Click the following link or paste it in your browser to set a password and activate your user. https://www.pureokrs.com/resetpassword/'+ email + '/' + token,
-    html: 'Click the following link to set a password and activate your user. <a href="https://www.pureokrs.com/resetpassword/'+ email + '/' + token +'">Activate User</a>',
+    text: `
+            Click the following link or paste it in your browser to set a password and activate your user. 
+            https://www.pureokrs.com/resetpassword/${email}/${token}
+    `,
+    html: `
+            <body style="min-height: 70vh;">
+              <div 
+              style="
+                background-color:#007bff;
+                height:40px;">
+              </div>
+              <div style="
+                background-color:#343a40;
+                color:#f8f9fa;
+                padding: 10px;">
+                <h3 style="color:#f8f9fa">You now have a user at PureOKRs. Welcome!</h3>
+                <p
+                  style="
+                    color:#f8f9fa;
+                    "
+                >
+                  Click the following button to set a password and activate your user.
+                </p>
+                <a href="https://www.pureokrs.com/resetpassword/${email}/${token}" 
+                  style="
+                    text-decoration:none;
+                    color:#f8f9fa;
+                    background-color:#007bff;
+                    padding: 2px 6px 2px 6px;
+                    border-radius: 2px;"
+                >
+                  Activate User
+                </a>
+              </div>
+              <div 
+                  style="
+                    background-color:#007bff;
+                    height:20px;">
+              </div>
+            </body>
+    `,
   };
-  msg.subject = 'You now have a user at pureOKRs. Welcome!';
+  msg.subject = 'You now have a user at PureOKRs. Welcome!';
   msg.to = email;
   sgMail.setApiKey(process.env.SENDGRID_API_KEY);
-  sgMail.send(msg, (err, doc) =>{});
+  sgMail.send(msg, (err, doc) => { });
   return;
 }
 
 module.exports.sendResetEmail = (email, token) => {
   let msg = {
     from: 'teodor.nilseng@gmail.com',
-    text: 'Click the following link or paste it in your browser to reset your password. https://www.pureokrs.com/resetpassword/'+ email + '/' + token,
-    html: 'Click the following link to reset your password. <a href="https://www.pureokrs.com/resetpassword/'+ email + '/' + token +'">Reset</a>',
+    text: 'Click the following link or paste it in your browser to reset your password. https://www.pureokrs.com/resetpassword/' + email + '/' + token,
+    html: 'Click the following link to reset your password. <a href="https://www.pureokrs.com/resetpassword/' + email + '/' + token + '">Reset</a>',
   };
   msg.subject = 'You can now reset your password at pureOKRs.';
   msg.to = email;
   sgMail.setApiKey(process.env.SENDGRID_API_KEY);
-  sgMail.send(msg, (err, doc) =>{});
+  sgMail.send(msg, (err, doc) => { });
   return;
 }
