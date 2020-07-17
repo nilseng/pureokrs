f187c9eca82105e54da72f1bf39cbefbd80a9555
Teodor Nilseng Danielsen <teodor.danielsen@gmail.com>
2020-07-17 22:37:39 +0200

    adjusting min-height on main element to fit screen better

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -4,7 +4,8 @@ import { AuthenticationService } from "../authentication.service"
 
 @Component({
     selector: 'app-navbar',
-    templateUrl: './navbar.component.html'
+    templateUrl: './navbar.component.html',
+    styleUrls: ['./navbar.component.scss']
 })
 export class NavbarComponent {
     faStream = faStream
