8394d3354f83061611921179acbeec9ca11196d1
Teodor Nilseng Danielsen <teodor.danielsen@gmail.com>
2020-07-19 19:51:33 +0200

    removing unused cookie-parser package

--- server.js
+++ server.js
@@ -1,5 +1,4 @@
 const express = require("express");
-const cookieParser = require("cookie-parser");
 const bodyParser = require("body-parser");
 const passport = require("passport");
 const dotenv = require("dotenv");
