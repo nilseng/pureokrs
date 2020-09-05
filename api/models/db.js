09f005bd3b741aa030299dada4830bbf4f789498
Teodor Nilseng Danielsen <teodor.danielsen@gmail.com>
2020-09-05 15:52:25 +0200

    updating db env variable name

--- api/models/db.js
+++ api/models/db.js
@@ -3,7 +3,10 @@ const dotenv = require("dotenv");
 
 dotenv.config();
 
-const dbURI = process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/pureokrs";
+const dbURI =
+  process.env.DB_URI ||
+  process.env.MONGODB_URI ||
+  "mongodb://127.0.0.1:27017/pureokrs";
 
 mongoose.connect(dbURI, {
   useNewUrlParser: true,
