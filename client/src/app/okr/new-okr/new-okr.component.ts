2871a63dd1e1c7775a45613028f04b3607cb8206
nilseng <teodor.nilseng@gmail.com>
2022-01-11 23:48:28 +0100

    implementing glassmorphism for new okr modal

--- client/src/app/okr/new-okr/new-okr.component.ts
+++ client/src/app/okr/new-okr/new-okr.component.ts
@@ -1,47 +1,57 @@
-import { Component, OnInit, Input, Output, EventEmitter, OnChanges, ViewChild, ElementRef } from '@angular/core'
-import { Router, ActivatedRoute } from '@angular/router'
-import { Observable, Subject } from 'rxjs'
 import {
-  debounceTime, distinctUntilChanged, switchMap
-} from 'rxjs/operators'
-import { faPlusCircle, faTrashAlt } from '@fortawesome/free-solid-svg-icons'
+  Component,
+  OnInit,
+  Input,
+  Output,
+  EventEmitter,
+  OnChanges,
+  ViewChild,
+  ElementRef,
+  AfterViewInit,
+} from "@angular/core";
+import { Router, ActivatedRoute } from "@angular/router";
+import { Observable, Subject } from "rxjs";
+import { debounceTime, distinctUntilChanged, switchMap } from "rxjs/operators";
+import { faPlusCircle, faTrashAlt } from "@fortawesome/free-solid-svg-icons";
 
-import { Okr, KeyResult } from '../okr'
-import { OkrService } from '../../okr.service'
-import { AuthenticationService, UserDetails } from '../../authentication.service'
-import { UserService } from '../../user.service'
-import { OkrNode } from '../okr-node'
+import { Okr, KeyResult } from "../okr";
+import { OkrService } from "../../okr.service";
+import {
+  AuthenticationService,
+  UserDetails,
+} from "../../authentication.service";
+import { UserService } from "../../user.service";
+import { OkrNode } from "../okr-node";
 
 @Component({
-  selector: 'app-new-okr',
-  templateUrl: './new-okr.component.html',
-  styleUrls: ['./new-okr.component.css']
+  selector: "app-new-okr",
+  templateUrl: "./new-okr.component.html",
+  styleUrls: ["./new-okr.component.css"],
 })
 export class NewOkrComponent implements OnInit, OnChanges {
-  faPlusCircle = faPlusCircle
-  faTrashAlt = faTrashAlt
+  faPlusCircle = faPlusCircle;
+  faTrashAlt = faTrashAlt;
 
-  @Input() parentId: string
-  @Output() savedOkr = new EventEmitter<OkrNode>()
-  @Output() clearParent = new EventEmitter()
+  @Input() parentId: string;
+  @Output() savedOkr = new EventEmitter<OkrNode>();
+  @Output() clearParent = new EventEmitter();
 
-  @ViewChild('parentSearchBox', { static: false }) parentSearchEl: ElementRef
-  @ViewChild('ownerSearchBox', { static: false }) ownerSearchEl: ElementRef
+  @ViewChild("objectiveInput") objectiveInputEl: ElementRef;
 
-  okr: Okr
-  krCount: number
+  okr: Okr;
+  krCount: number;
 
-  noObjective: boolean
+  noObjective: boolean;
 
   //Variables for searching for OKR owner
-  users$: Observable<{}>
-  private ownerSearchTerms = new Subject<string>()
-  owner: UserDetails
+  users$: Observable<{}>;
+  private ownerSearchTerms = new Subject<string>();
+  owner: UserDetails;
 
   //Variables for searching for OKR parent
-  parents$: Observable<{}>
-  private parentSearchTerms = new Subject<string>()
-  parent: Okr
+  parents$: Observable<{}>;
+  private parentSearchTerms = new Subject<string>();
+  parent: Okr;
 
   constructor(
     private okrService: OkrService,
@@ -49,127 +59,128 @@ export class NewOkrComponent implements OnInit, OnChanges {
     private userService: UserService,
     private route: ActivatedRoute,
     private router: Router
-  ) { }
+  ) {}
 
   ngOnInit() {
-    this.okr = new Okr('')
-    this.okr.keyResults.push(new KeyResult(''))
-    this.auth.getUserDetails().subscribe(
-      u => {
-        this.owner = u
-        this.okr.userId = u._id
-      }
-    )
+    this.okr = new Okr("");
+    this.okr.keyResults.push(new KeyResult(""));
+    this.auth.getUserDetails().subscribe((u) => {
+      this.owner = u;
+      this.okr.userId = u._id;
+    });
 
-    this.getParent()
+    this.getParent();
 
-    this.noObjective = false
+    this.noObjective = false;
 
     this.users$ = this.ownerSearchTerms.pipe(
       debounceTime(300),
       distinctUntilChanged(),
-      switchMap((term: string) => this.userService.searchUsers(term)),
-    )
+      switchMap((term: string) => this.userService.searchUsers(term))
+    );
 
     this.parents$ = this.parentSearchTerms.pipe(
       debounceTime(300),
       distinctUntilChanged(),
-      switchMap((term: string) => this.okrService.searchOkrs(term)),
-    )
+      switchMap((term: string) => this.okrService.searchOkrs(term))
+    );
   }
 
   ngOnChanges() {
-    this.okr = new Okr('')
-    this.okr.objective = ''
-    this.okr.keyResults = []
-    this.okr.keyResults.push(new KeyResult(''))
-    this.auth.getUserDetails().subscribe(
-      u => {
-        this.owner = u
-        this.okr.userId = u._id
-      }
-    )
-    this.noObjective = false
-    this.getParent()
+    this.okr = new Okr("");
+    this.okr.objective = "";
+    this.okr.keyResults = [];
+    this.okr.keyResults.push(new KeyResult(""));
+    this.auth.getUserDetails().subscribe((u) => {
+      this.owner = u;
+      this.okr.userId = u._id;
+    });
+    this.noObjective = false;
+    this.getParent();
   }
 
   ownerSearch(term: string): void {
-    this.ownerSearchTerms.next(term)
+    this.ownerSearchTerms.next(term);
   }
 
   parentSearch(term: string): void {
-    this.parentSearchTerms.next(term)
+    this.parentSearchTerms.next(term);
   }
 
   save(): void {
-    this.parentSearch('')
-    this.ownerSearch('')
+    this.parentSearch("");
+    this.ownerSearch("");
     if (!this.okr.objective.trim()) {
-      this.noObjective = true
-      return
+      this.noObjective = true;
+      return;
     } else {
-      this.okrService.createOkr(this.okr)
-        .subscribe((okr: Okr) => {
-          if (okr.parent) {
-            this.addToParentOnSave(okr)
-          } else {
-            this.savedOkr.emit(new OkrNode(okr))
-          }
-          this.clearForm()
-        })
+      this.okrService.createOkr(this.okr).subscribe((okr: Okr) => {
+        if (okr.parent) {
+          this.addToParentOnSave(okr);
+        } else {
+          this.savedOkr.emit(new OkrNode(okr));
+        }
+        this.clearForm();
+      });
     }
   }
 
   getParent() {
     if (this.parentId) {
-      this.okrService.getOkr(this.parentId)
-        .subscribe((okr: Okr) => {
-          this.parent = okr
-          this.okr.parent = okr._id
-        })
+      this.okrService.getOkr(this.parentId).subscribe((okr: Okr) => {
+        this.parent = okr;
+        this.okr.parent = okr._id;
+      });
     }
   }
 
   addKeyResult(): void {
-    this.okr.keyResults.push(new KeyResult(''))
+    this.okr.keyResults.push(new KeyResult(""));
   }
 
   removeKeyResult(id: string, index: number): void {
-    this.okr.keyResults.splice(index, 1)
+    this.okr.keyResults.splice(index, 1);
   }
 
   addToParentOnSave(okr: Okr) {
-    this.okrService.addChild(okr.parent, okr._id)
-      .subscribe(() => {
-        this.savedOkr.emit(new OkrNode(okr))
-      })
+    this.okrService.addChild(okr.parent, okr._id).subscribe(() => {
+      this.savedOkr.emit(new OkrNode(okr));
+    });
   }
 
   assign(owner: UserDetails): void {
-    this.okr.userId = owner._id
-    this.owner = owner
-    this.ownerSearch('')
+    this.okr.userId = owner._id;
+    this.owner = owner;
+    this.ownerSearch("");
   }
 
   link(parent: Okr): void {
-    this.okr.parent = parent._id
-    this.parent = parent
-    this.parentSearch('')
+    this.okr.parent = parent._id;
+    this.parent = parent;
+    this.parentSearch("");
   }
 
   clearForm() {
-    this.okr = new Okr('')
-    this.okr.objective = ''
-    this.okr.keyResults = []
-    this.okr.keyResults.push(new KeyResult(''))
-    this.auth.getUserDetails().subscribe(
-      u => {
-        this.owner = u
-        this.okr.userId = u._id
-      }
-    )
-    this.parent = undefined
-    this.clearParent.emit()
-    this.noObjective = false
+    this.okr = new Okr("");
+    this.okr.objective = "";
+    this.okr.keyResults = [];
+    this.okr.keyResults.push(new KeyResult(""));
+    this.auth.getUserDetails().subscribe((u) => {
+      this.owner = u;
+      this.okr.userId = u._id;
+    });
+    this.parent = undefined;
+    this.clearParent.emit();
+    this.noObjective = false;
+  }
+
+  focusObjectiveInput() {
+    setTimeout(() => this.objectiveInputEl?.nativeElement.focus(), 100);
+  }
+
+  // Called when toggling the modal
+  initForm() {
+    this.clearForm();
+    this.focusObjectiveInput();
   }
 }
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,4 +1,5 @@
 module.exports = {
+  mode: "jit",
   purge: { enabled: true, content: ["./src/**/*.{html,ts}"] },
   darkMode: false, // or 'media' or 'class'
   theme: {
